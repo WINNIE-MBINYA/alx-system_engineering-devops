85e182093f1b1de2db8c618da26f9b4bf27a34ec
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-05-17 01:09:58 +0300

    Add files

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,14 @@
+# Adjusting file limits to enable the user holberton to login and open files without errors
+
+# Increase hard file limit for holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Adjusting soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
